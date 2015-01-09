05f5525f9013dc872b4d840f14b1e2a322433e6b
Eric Gelinas <evg@stamen.com>
2015-01-09 14:57:11 -0800

    add source

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,7 +8,8 @@ var gulp           = require('gulp'),
 
 var paths = {
   js        : './src/*.js',
-  publicJs  : './'
+  publicJs  : './',
+  dist      : './dist'
 };
 
 //
@@ -18,28 +19,11 @@ var paths = {
 gulp.task('lint', function() {
   return gulp.src(paths.js)
   .pipe(jshint({
-    "predef" : [
-    "define",
-    "document",
-    "location",
-    "navigator",
-    "window",
-    "history"
-    ],
-    "expr" : true
+    "predef"       : ["module"],
+    "expr"         : true,
+    "globalstrict" : true
   }))
   .pipe(jshint.reporter('jshint-stylish'));
-});
-
-//
-// Minify JS and move it to the
-// public directory
-//
-gulp.task('copyjs', function() {
-
-  gulp.src(paths.js)
-  .pipe(copy("./public"))
-
 });
 
 gulp.task('uglify', function() {
@@ -54,7 +38,7 @@ gulp.task('uglify', function() {
   }))
   .pipe(rename({extname: ".min.js"}))
   .pipe(sourcemaps.write("./")) //Write a sourcemap for browser debugging
-  .pipe(gulp.dest(paths.publicJs))
+  .pipe(gulp.dest(paths.dist))
 });
 
 //
@@ -62,7 +46,6 @@ gulp.task('uglify', function() {
 //
 gulp.task('default',function(){
   gulp.start('lint');
-  gulp.start('copyjs');
   gulp.start('uglify');
 });
 
@@ -70,6 +53,6 @@ gulp.task('default',function(){
 // Watch directories For Changes
 //
 gulp.task('watch', function() {
-  gulp.watch(paths.js, ['lint','copyjs','uglify']);
+  gulp.watch(paths.js, ['lint','uglify']);
   console.log('watching directory:' + paths.js);
 });
