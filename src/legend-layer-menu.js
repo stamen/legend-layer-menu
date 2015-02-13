76a921710852f2d15d75aa16bd4b911ae3b0606a
Eric Gelinas <evg@stamen.com>
2015-02-13 10:49:52 -0800

    add color picking

--- src/legend-layer-menu.js
+++ src/legend-layer-menu.js
@@ -1,7 +1,9 @@
 (function() {
   "use strict";
 
-  function LegendLayerMenu(rootSelector) {
+  function LegendLayerMenu(rootSelector, options) {
+
+    options = options || {};
 
     //
     // Constants
@@ -10,6 +12,37 @@
         rootNode          = document.querySelector(rootSelector), //"#legend-layer-menu",
         layerGroups       = {},
         layers            = {},
+        colors            = ["#2166ac","#b2182b","#f4a582","#4393c3","#67001f","#d1e5f0","#fddbc7","#f7f7f7","#d6604d","#92c5de","#053061"],
+        secondaryColors   = [
+          "#2166ac","#b2182b","#f4a582","#4393c3","#67001f","#d1e5f0","#fddbc7","#f7f7f7","#d6604d","#92c5de","#053061","#8dd3c7","#ffffb3",
+          "#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5","#ffed6f",
+          "#66c2a5","#fc8d62","#8da0cb","#e78ac3","#a6d854","#ffd92f","#e5c494","#b3b3b3",
+          "#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00","#ffff33","#a65628","#f781bf","#999999",
+          "#b3e2cd","#fdcdac","#cbd5e8","#f4cae4","#e6f5c9","#fff2ae","#f1e2cc","#cccccc",
+          "#fbb4ae","#b3cde3","#ccebc5","#decbe4","#fed9a6","#ffffcc","#e5d8bd","#fddaec","#f2f2f2",
+          "#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#ffff99","#b15928",
+          "#1b9e77","#d95f02","#7570b3","#e7298a","#66a61e","#e6ab02","#a6761d","#666666",
+          "#7fc97f","#beaed4","#fdc086","#ffff99","#386cb0","#f0027f","#bf5b17","#666666",
+          "#a50026","#d73027","#f46d43","#fdae61","#fee08b","#ffffbf","#d9ef8b","#a6d96a","#66bd63","#1a9850","#006837",
+          "#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#ffffbf","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2",
+          "#a50026","#d73027","#f46d43","#fdae61","#fee090","#ffffbf","#e0f3f8","#abd9e9","#74add1","#4575b4","#313695",
+          "#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#ffffff","#e0e0e0","#bababa","#878787","#4d4d4d","#1a1a1a",
+          "#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061",
+          "#8e0152","#c51b7d","#de77ae","#f1b6da","#fde0ef","#f7f7f7","#e6f5d0","#b8e186","#7fbc41","#4d9221","#276419",
+          "#40004b","#762a83","#9970ab","#c2a5cf","#e7d4e8","#f7f7f7","#d9f0d3","#a6dba0","#5aae61","#1b7837","#00441b",
+          "#543005","#8c510a","#bf812d","#dfc27d","#f6e8c3","#f5f5f5","#c7eae5","#80cdc1","#35978f","#01665e","#003c30",
+          "#7f3b08","#b35806","#e08214","#fdb863","#fee0b6","#f7f7f7","#d8daeb","#b2abd2","#8073ac","#542788","#2d004b",
+          "#ffffff","#f0f0f0","#d9d9d9","#bdbdbd","#969696","#737373","#525252","#252525","#000000",
+          "#fff5f0","#fee0d2","#fcbba1","#fc9272","#fb6a4a","#ef3b2c","#cb181d","#a50f15","#67000d",
+          "#fff5eb","#fee6ce","#fdd0a2","#fdae6b","#fd8d3c","#f16913","#d94801","#a63603","#7f2704",
+          "#f7fcf5","#e5f5e0","#c7e9c0","#a1d99b","#74c476","#41ab5d","#238b45","#006d2c","#00441b",
+          "#f7fbff","#deebf7","#c6dbef","#9ecae1","#6baed6","#4292c6","#2171b5","#08519c","#08306b",
+          "#fcfbfd","#efedf5","#dadaeb","#bcbddc","#9e9ac8","#807dba","#6a51a3","#54278f","#3f007d",
+          "#ffffe5","#fff7bc","#fee391","#fec44f","#fe9929","#ec7014","#cc4c02","#993404","#662506",
+          "#ffffcc","#ffeda0","#fed976","#feb24c","#fd8d3c","#fc4e2a","#e31a1c","#bd0026","#800026"
+        ],
+        defaultColor      = options.color || "#000",
+        layerLimit        = options.layerLimit || colors.length,
         sortIcon          = [
               "<svg version=1.1 class=\"drag-icon\" xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink x=0px y=0px viewBox=\"-568.5 362.1 61.6 46.3\" enable-background=\"new -568.5 362.1 61.6 46.3\" xml:space=preserve class=\"grab\"><g><path d=\"M-507.3,370.4l-7.7-7.9c0,0,0,0,0,0c-0.1-0.1-0.3-0.3-0.5-0.4c0,0-0.1,0-0.1,0c0,0-0.1,0-0.1,0c-0.2,0-0.3-0.1-0.5-0.1",
               "c0,0,0,0,0,0c0,0,0,0,0,0c-0.2,0-0.3,0-0.5,0.1c0,0-0.1,0-0.1,0c-0.2,0.1-0.4,0.2-0.6,0.4l-7.7,7.9c-0.6,0.7-0.6,1.7,0,2.3",
@@ -30,9 +63,10 @@
               "L74,44.1l33.6-33.6l4.2-4.2l-8.4-8.4l-4.2,4.2L65.7,35.7L32.1,2.1C32.1,2.1,27.9-2.1,27.9-2.1z\"/>",
               "</svg>"
         ].join(""),
-        layerTemplate     = "<li class=\"draggable drag-drop layer-item-{id}\" data-id=\"{id}\"> " + sortIcon + "<span class=\"label\">{label}</span> " + deleteIcon + " </li>",
-        inputFormTemplate = "<div class=\"input-form hidden\"><form class=\"input-form-element\" name=\"{layerid}-input-form\"><input type=\"text\" name=\"uri\" placeholder=\"EcoEnginwel API URI\" value=\"https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=family_exact%3A%22cricetidae%22&q=&min_date=1960&max_date=1965&page_size=100\"><input type=\"text\" name=\"label\" placeholder=\"A name for this layer\"><button>Add</button></form></div>",
-        i, dragConfig, oldParent, dropConfig, orderCache;
+        layerTemplate       = "<li class=\"draggable drag-drop layer-item-{id}\" data-id=\"{id}\" data-color=\"{color}\" data-list=\"{group}\" data-label=\"{label}\" data-uri=\"{uri}\"> " + sortIcon + "<input type=\"color\" class=\"color-picker\" value=\"{color}\"><span class=\"label\">{label}</span> " + deleteIcon + " </li>",
+        inputFormTemplate   = "<div class=\"input-form hidden\"><form class=\"input-form-element\" name=\"{layerid}-input-form\"><input type=\"text\" name=\"uri\" placeholder=\"EcoEnginwel API URI\" value=\"https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=family_exact%3A%22cricetidae%22&q=&min_date=1960&max_date=1965&page_size=100\"><input type=\"text\" name=\"label\" placeholder=\"A name for this layer\"><button>Add</button></form></div>",
+        colorPickerTemplate = "<div class=\"color-picker-panel\" style=\"display:none;position:absolute;\">" + colors.map(function(c) {return "<div class=\"color\" style=\"background-color:"+c+";\"></div>"}).join("") + "</div>",
+        i, dragConfig, oldParent, dropConfig, orderCache, inputFormNode;
 
     //
     // Add a default class name
@@ -54,12 +88,6 @@
         //TODO: Stop hard coding the offset
         event.target.setAttribute("data-x", (event.clientX-event.target.offsetLeft+10)-(event.clientX-oLeft));
         event.target.setAttribute("data-y", (event.clientY-event.target.offsetTop+60)-(event.clientY-oTop));
-
-        /*
-        event.target.style.webkitTransform =
-        event.target.style.transform =
-        "translate(" + (event.pageX-event.target.offsetLeft) + "px, " + (event.pageY-event.target.offsetTop) + "px)";
-        */
 
         event.target.classList.add("dragging");
       },
@@ -176,6 +204,42 @@
       }
     };
 
+    //
+    // Finds a color from the colors array which is not
+    // already being used. If they are all being used, it
+    // returns the default color
+    //
+    function getNewColor() {
+
+      var usedColors = [],
+          element;
+
+      for (var i in layers) {
+        if ( layers.hasOwnProperty(i) ) {
+
+          element = layers[i].element();
+
+          if (element && element.getAttribute("data-color")) {
+            usedColors.push(element.getAttribute("data-color"));
+          }
+
+        }
+      }
+
+      for (var i=0; colors.length > i; i++) {
+        if ( usedColors.indexOf( colors[ i ] ) < 0 ) {
+          return colors[i];
+        }
+      }
+
+      return defaultColor;
+
+    }
+    window.getNewColor = getNewColor;
+
+    //
+    // Emits the orderChanged event
+    //
     function triggerOrderChange() {
       var order = getLayerOrder();
 
@@ -189,12 +253,7 @@
           //
 
           if (!interactObject.list) {
-            return {
-              "id"    : interactObject.element().getAttribute("data-id"),
-              "list"  : interactObject.element().getAttribute("data-list"),
-              "label" : interactObject.element().getAttribute("data-label"),
-              "uri"   : interactObject.element().getAttribute("data-uri")
-            }
+            return getLayerObjectFromLayerElement(interactObject.element());
           } else {
             return interactObject;
           }
@@ -207,6 +266,12 @@
       }
     }
 
+    //
+    // Return layers in order. Currently deturmined by their order
+    // in the DOM.
+    // TODO: optionally read order from a data attribute on the layer
+    //       element
+    //
     function getLayerOrder() {
       var layerNodes   = rootNode.querySelectorAll(".draggable"),
           rasterLayers = rootNode.querySelectorAll("select"),
@@ -270,6 +335,9 @@
         return newId;
     }
 
+    //
+    // Appends a markup string to the DOM as DOM elements
+    //
     function append(rootNode, html) {
       var div = document.createElement("div");
       div.innerHTML = html;
@@ -280,6 +348,11 @@
       return rootNode;
     }
 
+    //
+    // Return the parent of this element which has the passed class. if
+    // the startingElement has the class it will be returned. The depth
+    // defaults to 10
+    //
     function parentHasClass(startingElement, className, depth) {
 
       var last  = startingElement;
@@ -311,7 +384,7 @@
 
     function promptUserForLayerData(buttonNode, callback) {
 
-      var inputFormNode = buttonNode.parentNode.querySelector(".input-form");
+      inputFormNode = buttonNode.parentNode.querySelector(".input-form");
 
       //
       // Rise above!
@@ -368,13 +441,14 @@
     }
 
     function createLayer (layerNode) {
+
       layers[layerNode.getAttribute("data-id")] = interact(layerNode).draggable(dragConfig).dropzone(dropConfig);
 
-      layers[layerNode.getAttribute("data-id")].element().addEventListener("click", function(e) {
-
-        e.preventDefault();
+      layerNode.addEventListener("click", function(e) {
 
         if (e.target.classList.contains("delete-icon")) {
+
+          e.preventDefault();
 
           var layerElement = e.target.parentNode;
 
@@ -384,12 +458,22 @@
 
       });
 
-      that.fire("layerAdded", {
-        "list"  : layerNode.getAttribute("data-list"),
-        "label" : layerNode.getAttribute("data-label"),
-        "uri"   : layerNode.getAttribute("data-uri"),
-        "id"    : layerNode.getAttribute("data-id")
-      });
+      that.fire("layerAdded", getLayerObjectFromLayerElement(layerNode));
+    }
+
+    //
+    // Builds an object from data attributes in the layer
+    // element
+    //
+    function getLayerObjectFromLayerElement (element) {
+      return {
+        "list"    : element.getAttribute("data-list"),
+        "label"   : element.getAttribute("data-label"),
+        "uri"     : element.getAttribute("data-uri"),
+        "id"      : element.getAttribute("data-id"),
+        "color"   : element.getAttribute("data-color"),
+        "element" : element
+      };
     }
 
     function onClickLayerAddAction (e) {
@@ -406,14 +490,14 @@
           getLayerGroupNode(e.target.getAttribute("data-for")),
           processTemplate(layerTemplate, {
             "label" : formNode.label.value,
-            "id"    : layerId
+            "id"    : layerId,
+            "color" : getNewColor(),
+            "group" : layerGroupId,
+            "uri"   : formNode.uri.value
           })
         );
 
         layerNode = rootNode.querySelector(".layer-item-" + layerId);
-        layerNode.setAttribute("data-list",  layerGroupId);
-        layerNode.setAttribute("data-label", formNode.label.value);
-        layerNode.setAttribute("data-uri",   formNode.uri.value);
 
         //
         // Register new layer
@@ -421,6 +505,64 @@
         createLayer(layerNode, layerGroupId);
 
       });
+    }
+
+    function initColorPicker() {
+
+      //
+      // Test for native color picker support
+      //
+      var input   = document.createElement("input"),
+          isSupported;
+
+      //
+      // Test for native color picker support
+      // Test inspired by https://bgrins.github.io/spectrum/
+      //
+      input.type  = "color";
+      input.value = "!";
+      isSupported = (input.type === "color" && input.type !== "!");
+
+      //
+      // If isSupported is true, this browser supports native color
+      // picking using the input[type=color] element
+      //
+      that.on("layerAdded", function(layer) {
+        var pickerNode = layer.caller.element.querySelector(".color-picker"),
+            panelNode  = document.querySelector(".color-picker-panel");
+
+        if (isSupported) {
+
+          pickerNode.addEventListener("change", function(e) {
+            e.target.parentNode.setAttribute("data-color", e.target.value);
+
+            that.fire("color-change", getLayerObjectFromLayerElement(e.target.parentNode));
+          });
+        } else {
+
+          pickerNode.classList.add("polyfill");
+          pickerNode.style.backgroundColor = pickerNode.value;
+          pickerNode.style.cursor = "pointer";
+          pickerNode.style.color = "transparent";
+
+          if (!panelNode) {
+            append(rootNode, processTemplate(colorPickerTemplate, {}));
+            panelNode  = document.querySelector(".color-picker-panel");
+          }
+
+          pickerNode.addEventListener("click", function(e) {
+
+            e.target.parentNode.parentNode.insertBefore(panelNode, e.target.parentNode);
+            e.target.parentNode.parentNode.insertBefore(e.target.parentNode, panelNode); //TODO: There is probably a better way to flip these around
+            panelNode.style.top     = (e.target.parentNode.offsetTop) + "px";
+            panelNode.style.left    = (e.target.parentNode.offsetLeft) + "px";
+            panelNode.style.display = "block";
+            panelNode.setAttribute("data-for", layer.caller.id);
+          });
+
+        }
+      });
+
     }
 
     function init() {
@@ -452,9 +594,29 @@
       // Handle click events
       //
       rootNode.addEventListener("click", function(e) {
+        var layerObject, layerNode;
 
-        if (e.target.tagName === "BUTTON") {
+        //
+        // Listener for add layer action
+        //
+        if (e.target.classList.contains("add-action")) {
           onClickLayerAddAction.apply(this, arguments);
+        }
+
+        //TODO: Turn this into a color input polyfill module
+        //
+        // Choose a color. This interface is only shown on browsers
+        // which do not support the color input type
+        //
+        if (e.target.classList.contains("color")) {
+          layerObject = layers[e.target.parentNode.getAttribute("data-for")];
+          layerNode   = layerObject.element();
+
+          layerNode.setAttribute("data-color", e.target.style.backgroundColor);
+          e.target.parentNode.style.display = "none";
+          layerNode.querySelector(".color-picker").setAttribute("value", e.target.style.backgroundColor);
+          layerNode.querySelector(".color-picker").style.backgroundColor = e.target.style.backgroundColor;
+          that.fire("color-change", getLayerObjectFromLayerElement(layerNode));
         }
 
       }, false);
@@ -480,6 +642,7 @@
     // Here we go
     //
 
+    initColorPicker();
     init();
 
     return that;
