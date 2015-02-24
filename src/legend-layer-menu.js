46caf6ff43316b38e416bcb62d7df65b1cb0c01f
Eric Gelinas <evg@stamen.com>
2015-02-24 14:31:40 -0800

    get edit working

--- src/legend-layer-menu.js
+++ src/legend-layer-menu.js
@@ -13,34 +13,6 @@
         layerGroups       = {},
         layers            = {},
         colors            = ["#2166ac","#b2182b","#f4a582","#4393c3","#67001f","#d1e5f0","#fddbc7","#f7f7f7","#d6604d","#92c5de","#053061"],
-        secondaryColors   = [
-          "#2166ac","#b2182b","#f4a582","#4393c3","#67001f","#d1e5f0","#fddbc7","#f7f7f7","#d6604d","#92c5de","#053061","#8dd3c7","#ffffb3",
-          "#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5","#ffed6f",
-          "#66c2a5","#fc8d62","#8da0cb","#e78ac3","#a6d854","#ffd92f","#e5c494","#b3b3b3",
-          "#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00","#ffff33","#a65628","#f781bf","#999999",
-          "#b3e2cd","#fdcdac","#cbd5e8","#f4cae4","#e6f5c9","#fff2ae","#f1e2cc","#cccccc",
-          "#fbb4ae","#b3cde3","#ccebc5","#decbe4","#fed9a6","#ffffcc","#e5d8bd","#fddaec","#f2f2f2",
-          "#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#ffff99","#b15928",
-          "#1b9e77","#d95f02","#7570b3","#e7298a","#66a61e","#e6ab02","#a6761d","#666666",
-          "#7fc97f","#beaed4","#fdc086","#ffff99","#386cb0","#f0027f","#bf5b17","#666666",
-          "#a50026","#d73027","#f46d43","#fdae61","#fee08b","#ffffbf","#d9ef8b","#a6d96a","#66bd63","#1a9850","#006837",
-          "#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#ffffbf","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2",
-          "#a50026","#d73027","#f46d43","#fdae61","#fee090","#ffffbf","#e0f3f8","#abd9e9","#74add1","#4575b4","#313695",
-          "#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#ffffff","#e0e0e0","#bababa","#878787","#4d4d4d","#1a1a1a",
-          "#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061",
-          "#8e0152","#c51b7d","#de77ae","#f1b6da","#fde0ef","#f7f7f7","#e6f5d0","#b8e186","#7fbc41","#4d9221","#276419",
-          "#40004b","#762a83","#9970ab","#c2a5cf","#e7d4e8","#f7f7f7","#d9f0d3","#a6dba0","#5aae61","#1b7837","#00441b",
-          "#543005","#8c510a","#bf812d","#dfc27d","#f6e8c3","#f5f5f5","#c7eae5","#80cdc1","#35978f","#01665e","#003c30",
-          "#7f3b08","#b35806","#e08214","#fdb863","#fee0b6","#f7f7f7","#d8daeb","#b2abd2","#8073ac","#542788","#2d004b",
-          "#ffffff","#f0f0f0","#d9d9d9","#bdbdbd","#969696","#737373","#525252","#252525","#000000",
-          "#fff5f0","#fee0d2","#fcbba1","#fc9272","#fb6a4a","#ef3b2c","#cb181d","#a50f15","#67000d",
-          "#fff5eb","#fee6ce","#fdd0a2","#fdae6b","#fd8d3c","#f16913","#d94801","#a63603","#7f2704",
-          "#f7fcf5","#e5f5e0","#c7e9c0","#a1d99b","#74c476","#41ab5d","#238b45","#006d2c","#00441b",
-          "#f7fbff","#deebf7","#c6dbef","#9ecae1","#6baed6","#4292c6","#2171b5","#08519c","#08306b",
-          "#fcfbfd","#efedf5","#dadaeb","#bcbddc","#9e9ac8","#807dba","#6a51a3","#54278f","#3f007d",
-          "#ffffe5","#fff7bc","#fee391","#fec44f","#fe9929","#ec7014","#cc4c02","#993404","#662506",
-          "#ffffcc","#ffeda0","#fed976","#feb24c","#fd8d3c","#fc4e2a","#e31a1c","#bd0026","#800026"
-        ],
         defaultColor      = options.color || "#000",
         layerLimit        = options.layerLimit || colors.length,
         sortIcon          = [
@@ -65,7 +37,7 @@
         ].join(""),
         editIcon        = "<span class=\"edit-icon\">[edit]</span>",
         layerTemplate       = "<li class=\"draggable drag-drop layer-item-{id}\" data-id=\"{id}\" data-color=\"{color}\" data-list=\"{list}\" data-label=\"{label}\" data-uri=\"{uri}\"> " + sortIcon + "<input type=\"color\" class=\"color-picker\" value=\"{color}\"><span class=\"label\">{label}</span> " + deleteIcon + editIcon + " </li>",
-        inputFormTemplate   = "<div class=\"input-form hidden\"><form class=\"input-form-element\" name=\"{layerid}-input-form\"><input type=\"text\" name=\"uri\" placeholder=\"EcoEnginwel API URI\" value=\"https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=family_exact%3A%22cricetidae%22&q=&min_date=1960&max_date=1965&page_size=100\"><input type=\"text\" name=\"label\" placeholder=\"A name for this layer\"><button>Add</button></form></div>",
+        inputFormTemplate   = "<div class=\"input-form hidden\"><form class=\"input-form-element\" name=\"{layerid}-input-form\"><input type=\"text\" name=\"uri\" placeholder=\"EcoEnginwel API URI\" value=\"https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=family_exact%3A%22cricetidae%22&q=&min_date=1960&max_date=1965&page_size=100\"><input type=\"text\" name=\"label\" placeholder=\"A name for this layer\"><input type=\"hidden\" name=\"id\"><button>Add</button></form></div>",
         colorPickerTemplate = "<div class=\"color-picker-panel\" style=\"display:none;position:absolute;\">" + colors.map(function(c) {return "<div class=\"color\" style=\"background-color:"+c+";\"></div>"}).join("") + "</div>",
         i, dragConfig, oldParent, dropConfig, orderCache, inputFormNode;
 
@@ -236,7 +208,6 @@
       return defaultColor;
 
     }
-    window.getNewColor = getNewColor;
 
     //
     // Emits the orderChanged event
@@ -384,17 +355,31 @@
 
     }
 
+    function getHeaderByFor(forString) {
+      var headers = rootNode.querySelectorAll("h2");
+
+      for (var i=0; headers.length > i; i++) {
+        if (headers[i].querySelector("button") && headers[i].querySelector("button").getAttribute("data-for") === forString) {
+          return headers[i];
+        }
+      }
+
+      return false;
+    }
+
     //
     // Prompts user for data for a  layer. If this is passed a button Node
     // it will create a layer, if it is a layer node, it will update it
     //
     function promptUserForLayerData(buttonNodeOrLayerNode, callback) {
 
-      var type = buttonNodeOrLayerNode.classList.contains("draggable") ? "edit" : "add",
-          formParent = (type === "add") ? buttonNodeOrLayerNode.parentNode : buttonNodeOrLayerNode.parentNode.parentNode.querySelector("h2"),
+      var type       = buttonNodeOrLayerNode.classList.contains("draggable") ? "edit" : "add",
+          formParent = (type === "add") ? buttonNodeOrLayerNode.parentNode : getHeaderByFor(buttonNodeOrLayerNode.getAttribute("data-list")),
           inputs;
 
-
+      //
+      // Get the input form for this group, if it exists
+      //
       inputFormNode = formParent.querySelector(".input-form");
 
       //
@@ -417,11 +402,9 @@
         }));
         inputFormNode = formParent.querySelector(".input-form");
 
-        if (type === "edit") {
-          inputFormNode.uri = buttonNodeOrLayerNode.getAttribute("data-uri");
-          inputFormNode.label = buttonNodeOrLayerNode.getAttribute("data-label");
-        }
-
+        //
+        // The submit button
+        //
         inputFormNode.querySelector("button").addEventListener("click", function(e) {
 
           e.preventDefault();
@@ -439,16 +422,20 @@
 
         }, false);
       }
+      var inputFormNodeElement = formParent.querySelector(".input-form-element");
 
       //
-      // Clear the form
+      // Set form default state
       //
-      inputs = inputFormNode.querySelectorAll("input");
-
-      for (var i=0; inputs.length > i; i++) {
-        inputs[i].value = "";
+      if (type === "edit") {
+        inputFormNodeElement.uri.value   = buttonNodeOrLayerNode.getAttribute("data-uri");
+        inputFormNodeElement.label.value = buttonNodeOrLayerNode.getAttribute("data-label");
+        inputFormNodeElement.id.value    = buttonNodeOrLayerNode.getAttribute("data-id");
+      } else {
+        inputFormNodeElement.uri.value   = "";
+        inputFormNodeElement.label.value = "";
+        inputFormNodeElement.id.value    = "";
       }
-
 
       //
       // Show the form
@@ -504,12 +491,40 @@
       });
 
       layerNode.addEventListener("dblclick", function() {
-        promptUserForLayerData(layerNode, function() {
-          console.log(arguments);
+        promptUserForLayerData(layerNode, function(updates) {
+          var form = updates.querySelector("form");
+          updateLayerData(form.id.value, {
+            "uri"   : form.uri.value,
+            "id"    : form.id.value,
+            "label" : form.label.value
+          });
         });
       });
 
       that.fire("layerAdded", getLayerObjectFromLayerElement(layerNode));
+    }
+
+    function updateLayerData(id, properties) {
+      var element           = layers[id].element(),
+          updatedProperties = [];
+
+      for (var i in properties) {
+        if (properties.hasOwnProperty(i) && element.getAttribute("data-" + i) !== properties[i]) {
+
+          element.setAttribute("data-" + i, properties[i]);
+          updatedProperties.push(i);
+
+        }
+      }
+
+      element.querySelector(".label").innerHTML = properties.label;
+
+      if (updatedProperties.length) {
+        that.fire("layerUpdated", {
+          "layerObject"       : getLayerObjectFromLayerElement(element),
+          "updatedProperties" : updatedProperties
+        });
+      }
     }
 
     //
